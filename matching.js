e2edfbac10501ada1eabfbd7d483cff50fd8310b
Mponeng <mantombiratego@gmail.com>
2017-03-30 16:30:47 +0200

    updated webpage

---
+++ matching.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+function myfunction() {
+    var popup = document.getElementById("popUp");
+    popup.classList.toggle("sho");
+}
+
+
+function my2function() {
+    var popup = document.getElementById("popingUp");
+    popup.classList.toggle("shoi");
+}
