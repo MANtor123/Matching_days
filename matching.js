15d9163e09973b74e2e52ad2c2e2c5e22562f1ab
Mponeng <mantombiratego@gmail.com>
2017-03-31 16:40:55 +0200

    added functions

--- matching.js
+++ matching.js
@@ -1,3 +1,25 @@
+var date1 = document.querySelector('#date1');
+var date2 = document.querySelector('#date2');
+var button = document.getElementById('button');
+var weekday = document.querySelectorAll('.table')
+
+
+
+button.addEventListener('onclick', function(){
+
+for(var i=0; i < weekday.length; i++){
+ var week = weekday[i]
+if (date1.value === date2.value){
+
+}
+
+else if (! date1.value === date2.vale) {
+}
+
+
+}
+});
+
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
