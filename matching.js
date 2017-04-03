3436169202a2a66d4a177ac3c56581bdacc0d47f
Mponeng <mantombiratego@gmail.com>
2017-04-03 10:33:24 +0200

    updates webpage

--- matching.js
+++ matching.js
@@ -1,24 +1,44 @@
 var date1 = document.querySelector('#date1');
 var date2 = document.querySelector('#date2');
 var button = document.getElementById('button');
-var weekday = document.querySelectorAll('.table')
+var weekday = document.querySelectorAll('.table');
 
+//weekday.classList.add('color1','color2','color3');
 
+button.addEventListener('click', function(){
+  clear();
+var first = new Date(date1.value);
+var second = new Date(date2.value);
+var input1 = first.getDay();
+var input2 = second.getDay();
+if (input1 === input2) {
+  weekday[input1].classList.add('color1');
+}else {
+  weekday[input1].classList.add('color2');
+  weekday[input2].classList.add('color3');
+}
 
-button.addEventListener('onclick', function(){
-
-for(var i=0; i < weekday.length; i++){
- var week = weekday[i]
+/*for(var i=0; i < weekday.length; i++){
+ weekday[i]
 if (date1.value === date2.value){
-
+  return weekday[i] + color1;
 }
 
 else if (! date1.value === date2.vale) {
 }
 
 
-}
+}*/
 });
+
+var clear = function() {
+for (var i = 0; i < weekday.length; i++) {
+  weekday[i].classList.remove('color1');
+  weekday[i].classList.remove('color2');
+  weekday[i].classList.remove('color3');
+
+}
+}
 
 function myFunction() {
     var popup = document.getElementById("myPopup");
